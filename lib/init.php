e4c2b5e2ed607a1b5b76861ef376d9eefe78f852
Maurice Tadros <maurice@tadros.ca>
2019-05-19 16:43:39 -0600

    Added theme supoort for blocks and align-wide

--- lib/init.php
+++ lib/init.php
@@ -123,9 +123,7 @@ function beans_add_theme_support() {
 		)
 	);
 
-	if ( in_array( beans_get_layout(), array( 'c', 'sp_c_ss' ), true ) ) {
-		add_theme_support( 'align-wide' );
-	}
+	add_theme_support( 'align-wide' );
 	add_theme_support( 'wp-block-styles' );
 
 	// Beans specific.
